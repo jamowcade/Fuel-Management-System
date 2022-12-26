6cb0d65bc2a875bec28ad96ce0cf45d70d71b612
jaamow <ayanlestyle@gmail.com>
2022-12-26 14:25:48 +0300

    home view finished

---
+++ base/forms.py
@@ -0,0 +1,21 @@
+from django.forms import ModelForm
+from .models import Fuel, Stock, Sale
+
+
+
+class FuelForm(ModelForm):
+    class Meta:
+        model = Fuel
+        fields = ['name','price']
+
+
+class StockForm(ModelForm):
+    class Meta:
+        model = Stock
+        fields = ['fuel','volume']
+    
+
+class SaleForm(ModelForm):
+    class Meta:
+        model = Sale
+        fields = ['customer_name','fuel','volume']
--- base/views.py
+++ base/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
+from .forms import FuelForm, StockForm, SaleForm
+from .models import Fuel, Sale, Stock
+from django.db.models import Sum
 
 # Create your views here.
 
 
 def home(request):
+   
+    total_fuels = Fuel.objects.count()
+    fuels = Fuel.objects.all()
+
+    total_amount = Sale.objects.filter(fuel__id__in = fuels).aggregate(Sum('amount'))['amount__sum']
+    print(total_amount)
+    print(total_fuels)
     context = {
+        "total_fuel":total_fuels,
+        "total_sales": total_amount,
+        "fuels": fuels
+        
     }
     return render(request, 'base/home.html', context)

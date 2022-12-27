35f5f2490285886d030d007f493ffe7eb39113cf
jaamow <ayanlestyle@gmail.com>
2022-12-27 16:19:10 +0300

    ui updated

--- base/urls.py
+++ base/urls.py
@@ -8,5 +8,13 @@ urlpatterns = [
     path('stocks/', views.stockView, name="stocks"),
     path('sales/', views.saleView, name="sales"),
     path('inventory/', views.inventoryView, name="inventory"),
+    path('new_fuel/', views.saveFuel, name="new_fuel"),
+    path('fuel_details/<str:pk>/', views.fuelDetail, name="fuel_detail"),
+    path('new_stock/', views.saveStock, name="new_stock"),
+    path('new_sale/', views.saveSale, name="new_sale"),
+
+
+
+
 
 ]
